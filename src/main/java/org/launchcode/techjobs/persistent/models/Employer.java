e97095312cfea0e592630541f09f9588a564a2a6
cam <cameron.beebe87@yahoo.com>
2024-02-21 23:08:14 -0600

    things are being recorded by SQL, but nothing is being displayed once created

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,40 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+
+    @NotBlank(message = "Location is required")// Validation NO EMPTY
+    @Size(min = 3, max = 255)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer() { // no-arg constructor for Hibernate to make an object
+    }
+
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
